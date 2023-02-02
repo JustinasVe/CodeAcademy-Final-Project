2211bba7e4464d555268c3498ed1865f51bb9190
unknown <justinasve@gmail.com>
2023-02-02 04:54:28 +0200

    Database setup / used dotenv variables

--- app/app.js
+++ app/app.js
@@ -1,11 +1,23 @@
 const cors = require('cors');
 const express = require('express');
+const mysql = require('mysql2');
+
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = 8000;
+const mysqlConfig = {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
+};
 
+const connection = mysql.createConnection(mysqlConfig);
+
+const PORT = 8000;
 app.listen(PORT, () => console.log(`Express server is running on PORT: ${PORT}`));
